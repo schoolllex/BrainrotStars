c72d883bd52534232b0ebc200de7419d46314e15
schoolllex <llexllex001@gmail.com>
2026-03-23 12:02:45 +0100

    Update shop.js

--- shop/shop.js
+++ shop/shop.js
@@ -861,8 +861,6 @@ function renderInventory(data) {
         `;
         const openBtn = card.querySelector(".inventory-open-btn");
         openBtn.addEventListener("click", () => {
-            if(qty > 150)
-                return alert("La quantité maximum est de 100");
             openInventoryOpenModal(item, qty, card);
         });
         ui.inventoryGrid.appendChild(card);
